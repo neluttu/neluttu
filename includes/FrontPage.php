6c033a6ced7ae600a55932f8ffcfbbe7ad74ec1f
Neluttu <neluttu@gmail.com>
2023-01-31 01:17:20 +0200

    Added more projects

--- includes/FrontPage.php
+++ includes/FrontPage.php
@@ -5,7 +5,7 @@ $Projects = array(
                 'aalr' => array(
                             'title' => 'Asociația Artiștilor Lutieri din România',
                             'description' => 'Design concept for a violin makers association, front-end and back-end. Members database with CMS for easy updates.',
-                            'image' => 'aalr.ro.jpg',
+                            'image' => 'aalr.ro.png',
                             'url' => 'https://www.aalr.ro/',
                             'stack' => array(
                                             'html',
@@ -19,7 +19,7 @@ $Projects = array(
                 'aboutfood' => array(
                             'title' => 'aboutfood - online restaurants menus',
                             'description' => 'Personal project, mean to centralize all food menus in small cities. Complete user system with CMS for business registration.',
-                            'image' => 'aboutfood.ro.jpg',
+                            'image' => 'aboutfood.ro.png',
                             'url' => 'https://www.aboutfood.ro/',
                             'stack' => array(
                                             'html',
@@ -33,7 +33,7 @@ $Projects = array(
                 'bibliotecareghin' => array(
                             'title' => 'Petru Maior - Public City Library',
                             'description' => 'Concept and design for the city library, backed by a PHP driven CMS to edit dynamic pages. No frameworks involved.',
-                            'image' => 'BibliotecaReghin.ro.jpg',
+                            'image' => 'BibliotecaReghin.ro.png',
                             'url' => 'http://www.bibliotecareghin.ro/',
                             'stack' => array(
                                             'html',
@@ -47,7 +47,7 @@ $Projects = array(
                 'blumprod' => array(
                             'title' => 'Blum Prod - thermoforming plastics',
                             'description' => 'Concept and design for a thermoforming company. Presentation page, multilanguage suport and full SEO service.',
-                            'image' => 'Blumprod.ro.jpg',
+                            'image' => 'Blumprod.ro.png',
                             'url' => 'http://www.blumprod.ro/',
                             'stack' => array(
                                             'html',
@@ -60,7 +60,7 @@ $Projects = array(
                 'blueskystudios' => array(
                             'title' => 'Bluesky Studios S.R.L.',
                             'description' => 'Simple one page presentation website for my company. No special effects there. ',
-                            'image' => 'Blueskystudios.ro.jpg',
+                            'image' => 'Blueskystudios.ro.png',
                             'url' => 'http://www.blueskystudios.ro/',
                             'stack' => array(
                                             'html',
@@ -70,7 +70,7 @@ $Projects = array(
                 'bulgarasu' => array(
                             'title' => 'Bulgarasu Ski School and Camp',
                             'description' => 'Full featured website - user subscriptions, custom CMS, mailing system, online payments, special CMS for ski monitors. Full SEO service',
-                            'image' => 'Bulgarasu.ro.jpg',
+                            'image' => 'Bulgarasu.ro.png',
                             'url' => 'http://www.bulgarasu.ro/',
                             'stack' => array(
                                             'html',
@@ -81,6 +81,31 @@ $Projects = array(
                                             'mysql'
                                         )
                                     ),
+                'aces' => array(
+                            'title' => 'Poker Aces - CSS Project',
+                            'description' => 'A simple page with the for poker ace cards. All coded in CSS and HTML',
+                            'image' => 'CSS - Poker Aces.png',
+                            'url' => 'http://www.bulgarasu.ro/',
+                            'stack' => array(
+                                            'html',
+                                            'css'
+                                        )
+                                    ),
+                'DietEthic' => array(
+                            'title' => 'DietEthic - medical nutrition therapy',
+                            'description' => 'Concept and design for a medical nutrition therapy office. Full SEO service.',
+                            'image' => 'DietEthic.ro.png',
+                            'url' => 'http://www.bulgarasu.ro/',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                            'mysql'
+                                        )
+                                    ),
+
                 );
 print $Page;
 ?>
