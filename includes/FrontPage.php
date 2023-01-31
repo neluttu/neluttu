e4336c3963e67b31e596cfe81c42e875538c969d
Neluttu <neluttu@gmail.com>
2023-01-31 22:27:50 +0200

    Finished adding the projects to the array

--- includes/FrontPage.php
+++ includes/FrontPage.php
@@ -105,7 +105,169 @@ $Projects = array(
                                             'mysql'
                                         )
                                     ),
-
+                'dogmapub' => array(
+                            'title' => 'DogmaPub - online menu',
+                            'description' => 'Concept and design for a pub. Users can view on their mobile devices the pub\'s menu. CMS for easy price update, product edit / delete / add.',
+                            'image' => 'DogmaPub.ro.png',
+                            'url' => 'http://www.dogmapub.ro/',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                            'mysql'
+                                        )
+                                    ),
+                'HunterVipReghin' => array(
+                            'title' => 'Hunver VIP Reghin',
+                            'description' => 'Concept and design for a guest house, CMS for the online menu and client reservations included in the website.',
+                            'image' => 'HunterVipReghin.ro.png',
+                            'url' => 'http://www.huntervipreghin.ro/',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                            'mysql'
+                                        )
+                                    ),
+                'IncaltamintePiele' => array(
+                            'title' => 'Regsan - Shoes shop',
+                            'description' => 'Concept and design a direct from factory shoe show. Users can send a quick order. CMS for viewing orders, updating their statuses, client email notifications etc.',
+                            'image' => 'IncaltamintePiele.ro.png',
+                            'url' => 'http://www.huntervipreghin.ro/',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                            'mysql'
+                                        )
+                                    ),
+                'MareAurelian' => array(
+                            'title' => 'Mare Aurelian - violin maker',
+                            'description' => 'Presentation website for Mare Aurelian, violin maker. Design and impementation.',
+                            'image' => 'MareAurelian.ro.png',
+                            'url' => 'http://www.mareaurelian.ro/',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                        )
+                                    ),
+                'NBMed' => array(
+                            'title' => 'NBmed - private clinic',
+                            'description' => 'Presentation of a private medical clinic. Static website',
+                            'image' => 'NBMed.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                        )
+                                    ),
+                'Pansab' => array(
+                            'title' => 'Pansab - bakery and sweets shop',
+                            'description' => 'Design and implementation of an online sweets shop. Users can order directly by placing an order in their private accout. CMS for managing orders plus product management.',
+                            'image' => 'Pansab.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                            'mysql'
+                                        )
+                                    ),
+                'Prosan' => array(
+                            'title' => 'ProSano',
+                            'description' => 'Design and implementation of a snow shop - users can quick order fresh natural juice. All orders are email processed.',
+                            'image' => 'Pro-Sano.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                        )
+                                    ),
+                'StrindBerg' => array(
+                            'title' => 'Strindberg - winter sportswear',
+                            'description' => 'Design and implementation of a an online shop where users can buy winter sportswear. CMS for managing orders and products.',
+                            'image' => 'Strindberg.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'php',
+                                            'jquery',
+                                            'javascript',
+                                            'mysql'
+                                        )
+                                    ),
+                'UsersPage' => array(
+                            'title' => 'CSS project - users page',
+                            'description' => 'Designed this with pure HTML, CSS and JavaScript just for fun and future use.',
+                            'image' => 'Users_page.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'jquery',
+                                            'javascript',
+                                        )
+                                    ),
+                'medical' => array(
+                            'title' => 'Medical blood work tracker',
+                            'description' => 'Simple blood work tracker written in PHP and MySQL ',
+                            'image' => 'medical.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'jquery',
+                                            'javascript',
+                                            'php',
+                                            'mysql'
+                                        )
+                                    ),
+                'iNotes' => array(
+                            'title' => 'Personal WordPress Blog',
+                            'description' => 'Designed a theme for the wordpress platform; a personal blog that no longer exists.',
+                            'image' => 'iNotes.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'jquery',
+                                            'javascript',
+                                            'php',
+                                            'mysql'
+                                        )
+                                    ),
+                'pokeraces' => array(
+                            'title' => 'CSS project - Poker Aces',
+                            'description' => 'Saw this challange somewhere and decided to code the four aces in HTML and CSS.',
+                            'image' => 'Users_page.ro.png',
+                            'url' => '#',
+                            'stack' => array(
+                                            'html',
+                                            'css',
+                                            'jquery',
+                                            'javascript',
+                                            'php',
+                                            'mysql'
+                                        )
+                                    ),
                 );
 print $Page;
 ?>
